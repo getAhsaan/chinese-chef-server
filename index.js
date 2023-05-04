6fef2dca80a23e217c32fdf3181c5a3abded0fd7
getahsaan <getahsaan@gamil.com>
2023-05-04 11:09:23 +0600

    req chef send

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
+
+app.use(cors());
 
 // data
 const allChefsData = require("./chefs.json");
@@ -12,6 +15,12 @@ app.get("/", (req, res) => {
 
 app.get("/chefs", (req, res) => {
   res.send(allChefsData);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const chef_id = req.params.id;
+  const reqChef = allChefsData.find((chef) => chef.id == chef_id);
+  res.send(reqChef || "doesn't exist");
 });
 
 app.listen(port, () => {
