20ede49f30e58c31696792e9a9e2c8ef5baf1fe2
getahsaan <getahsaan@gamil.com>
2023-05-04 20:58:22 +0600

    deploy to netlify

---
+++ functions/api.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const router = express.Router();
+const cors = require("cors");
+
+app.use(cors());
+
+// data
+const allChefsData = require("../chefs.json");
+
+router.get("/", (req, res) => {
+  res.send("chinese chef hunter server is running..");
+});
+
+router.get("/chefs", (req, res) => {
+  res.send(allChefsData);
+});
+
+router.get("/chef/:id", (req, res) => {
+  const chef_id = req.params.id;
+  const reqChef = allChefsData.find((chef) => chef.id == chef_id);
+  res.send(reqChef || "doesn't exist");
+});
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
